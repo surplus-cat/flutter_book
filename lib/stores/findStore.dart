ac826fbe5a924eb4e0251d4851d976bfd20d305d
Corn <1073406997@qq.com>
2019-07-14 15:48:34 +0800

    add: Initial Project

---
+++ lib/stores/findStore.dart
@@ -0,0 +1,30 @@
+// import 'dart:convert';
+import 'package:flutter_book/api/http.dart';
+import 'package:dio/dio.dart';
+
+// Include generated file
+part 'findStore.g.dart';
+
+// This is the class used by rest of your codebase
+class FindStore = _FindStore with _$FindStore;
+
+// The store-class
+abstract class _$FindStore implements Store {
+  @observable
+  bool tile = false;
+
+  @observable
+  num count = 10;
+
+  @action
+  void setTile(String key, dynamic value) => tile = value;
+
+  @action
+  num counter() => this.count++;
+
+  // api 接口异步请求
+  void getInfo() async {
+    Response response =  await HttpRequest().dio.get('http://api.soscoon.com/playlist/hot');
+    print(response);
+  }
+}
---
+++ lib/widgets/Setting/SettingContent.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_book/helpers/constants.dart';
+
+class SettingContent extends StatelessWidget {
+  final List<Map<String, dynamic>> _listsItem = const [
+    {'value': '账号'},
+    {'value': '消息'},
+    {'value': '通知'},
+    {'value': '清除缓存'},
+    {'value': '反馈'},
+    {'value': '关于我们'}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView(
+      children: _listsItem.map((Map<String, dynamic> data) {
+        return ListTile (
+          title: Text(
+            data['value'],
+            style: TextStyle(color: Color(AppColors.fontColor), fontSize: 18.0),
+          ),
+          onTap: () {},
+          trailing: Icon(
+            Icons.keyboard_arrow_right,
+            color: Color(AppColors.fontColor)
+          )
+        );
+      }).toList()
+    );
+  }
+}
